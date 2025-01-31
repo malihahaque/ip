0d5634e6489a88637c9577de2779ba84deb7a670
Maliha Haque <e1130126@u.nus.edu>
2025-01-31 23:31:49 +0800

    add,list

--- src/main/java/Lara.java
+++ src/main/java/Lara.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class Lara {
     public static void main (String args[]) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
 
         System.out.println("Hello! I'm Lara");
         System.out.println("What can I do for you?");
@@ -13,9 +14,15 @@ public class Lara {
             if (message.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (message.equals("list")) {
+                for (int i=1; i<=list.size(); i++) {
+                    System.out.println(i+ ". " + list.get(i-1));
+                }
             } else {
-                System.out.println(message);
+                list.add(message);
+                    System.out.println("added: " + message);
             }
         }
+
     }
 }
