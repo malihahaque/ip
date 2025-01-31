881ac06cc2846cbdfbf38da971e8157efdf89b05
Maliha Haque <e1130126@u.nus.edu>
2025-01-31 23:08:50 +0800

    greetings

--- src/main/java/Lara.java
+++ src/main/java/Lara.java
@@ -1,7 +1,7 @@
 public class Lara {
     public static void main (String args[]) {
         System.out.println("Hello! I'm Lara");
-        System.out.println("What can I bejhwdj do for you?");
+        System.out.println("What can I do for you?");
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
