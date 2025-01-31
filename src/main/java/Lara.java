3f2a5c858dda2ce1410b4d9387b00ddd3ca8f75b
Maliha Haque <e1130126@u.nus.edu>
2025-01-31 23:16:17 +0800

    echo

--- src/main/java/Lara.java
+++ src/main/java/Lara.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Lara {
     public static void main (String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Lara");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while(true) {
+            String message = scanner.nextLine();
+            if (message.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(message);
+            }
+        }
+
     }
 }
