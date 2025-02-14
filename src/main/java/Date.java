0c8168e5fc9b4632ef3e1f596a016c01da83cd88
Maliha Haque <e1130126@u.nus.edu>
2025-02-14 15:42:44 +0800

    date & time

--- src/main/java/Date.java
+++ src/main/java/Date.java
@@ -1,10 +1,18 @@
 public class Date {
     public static Task Date(String input) {
-        String[] parts = input.split(" /by ");
-        String description = parts[0].substring(9).trim();
-        String date = parts[1].trim();
-
-        return new Deadline(description, date);
+        if (input.startsWith("deadline")) {
+            String[] parts = input.split(" /by ");
+            String description = parts[0].substring(9).trim();
+            String dateTime = parts[1].trim();
+            return new Deadline(description, dateTime);
+        } else if (input.startsWith("event")) {
+            String[] parts = input.split(" /from | /to ");
+            String description = parts[0].substring(6).trim();
+            String fromDateTime = parts[1].trim();
+            String toDateTime = parts[2].trim();
+            return new Event(description, fromDateTime, toDateTime);
+        }
+        return null;
     }
 }
 
