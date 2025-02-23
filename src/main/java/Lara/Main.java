bed84b7ca9fac81db0c6bf83d7b811041dc7f122
Maliha Haque <e1130126@u.nus.edu>
2025-02-24 01:46:25 +0800

    Fix bugs to prepare for release

--- src/main/java/Lara/Main.java
+++ src/main/java/Lara/Main.java
@@ -14,7 +14,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-    private Lara duke = new Lara("hello");
+    private Lara duke = new Lara("hello.txt");
 
     @Override
     public void start(Stage stage) {
